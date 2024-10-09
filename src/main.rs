acb42d7d64d63e5e23232d88e3fc6e5db2c9c424
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-09 22:54:03 +1300

    Implement State Pattern using Trait Objects for a blog post

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use rust_oop_state_pattern_blog_post::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+    
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+    
+}
